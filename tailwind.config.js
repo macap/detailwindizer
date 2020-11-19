3c3e7f3bcd3b3f0cb54e93c6815b2bc04dac9884
Maciek Caputa <maciek.caputa@gmail.com>
2020-11-19 18:15:49 +0100

    Initial (v1)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        white: 'red'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
